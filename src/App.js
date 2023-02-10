66f4e3d6a3f6317e5d5e1b170b3001e0c1b13fea
Abhishek Verma <abhishek1006.be20@chitkarauniversity.edu.in>
2023-02-10 15:31:30 +0530

    added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import LoginPage from './Pages/LoginPage/Login.page';
 import HomePage from './Pages/Homepage/Home.page';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends React.PureComponent{
   render(){
     return (
-      <Router>
-      <div className='App'>
+        <div className='App'>
           <Routes>
-            <Route exact path = '/' component = {HomePage} />
-            <Route exact path = '/login' component = {LoginPage} />
+            <Route path = '/' element = {<HomePage/>} />
+            <Route path = '/login' element = {<LoginPage/>}/>
           </Routes>
-      </div>
-      </Router>
+        </div>
     )
   }
 }
