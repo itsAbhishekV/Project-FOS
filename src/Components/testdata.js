42480de050dba05e5a09d9bcdc786e49bbc3d4ad
Dhruv Hizly <dhruvhizly940@gmail.com>
2023-02-11 05:14:16 +0530

    did some small changes

--- src/Components/testdata.js
+++ src/Components/testdata.js
@@ -8,6 +8,14 @@ const testdata = [
     {
         hashtag: "#thisistrend2",
         posts: [0, 1]
+    },
+    {
+        hashtag: "#thisistrend1",
+        posts: [0, 1, 2]
+    },
+    {
+        hashtag: "#thisistrend1",
+        posts: [0, 1, 2]
     }
 ]
 
