637882d1d6083de68193fc6ab93d96c31c2ad171
dhruvhizly <dhruvhizly940@gmail.com>
2023-02-12 01:41:48 +0530

    this is a backup

--- src/Components/WhatsHappening/WhatsHappening.components.jsx
+++ src/Components/WhatsHappening/WhatsHappening.components.jsx
@@ -13,7 +13,7 @@ const WhatsHappening = () => {
                     <img className='pfp' src='https://i.ibb.co/99xYxXw/Unknown-Profile-Picture.jpg' />
                 </div>
                 <form action='' method='post' className='form'>
-                    <textarea type='text' className='WH' name="whatsHappening" placeholder="What's Happening?" maxLength="100" />
+                    <textarea type='text' className='WH' name="whatsHappening" placeholder="What's Happening?" maxLength="280" />
                     <br /><br />
                     <input type='submit' value="Thot" className="newThot" />
                 </form>
