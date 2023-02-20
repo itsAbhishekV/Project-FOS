1dc170d772fa37b0841e2f9eb450b473ecc730ed
dhruvhizly <dhruvhizly940@gmail.com>
2023-02-21 00:12:32 +0530

    fixed some bugs and added a new thot modal

--- src/Components/Navbar/SideNavBar.Component.jsx
+++ src/Components/Navbar/SideNavBar.Component.jsx
@@ -7,12 +7,46 @@ import Person2RoundedIcon from '@mui/icons-material/Person2Rounded';
 import NewThotIcon from '../../Assets/2246837_new_tweet_write_icon.svg'
 import newLogo from '../../Assets/newLogo.png'
 import EmailIcon from '@mui/icons-material/Email';
+import Modal from 'react-overlays/Modal';
+import styled from '@emotion/styled';
+import maleavatar from '../../Assets/Male Avatar.svg';
+import CloseIcon from '@mui/icons-material/Close';
 
 const SideNavBar = () => {
 
     const [show, setShow] = useState(false);
 
+    const Backdrop = styled("div")`
+        position: fixed;
+        z-index: 1040;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: #000;
+        opacity: 0.5;
+    `;
+
+
+    const RandomlyPositionedModal = styled(Modal)`
+        position: fixed;
+        height: 100vh;
+        width: 100vw;
+        z-index: 1040;
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: rgba(255, 255, 255, 0.1);
+        backdrop-filter: blur(8px);
+        box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+    `;
+    
+    const renderBackdrop = ()=><Backdrop />
+
     return (
+        <>
         <div className='SideNavBar'>
             <Link to = '/' style={{ textDecoration: "none" }}>
                 <span className='inlineNav mainTitle'>
@@ -44,11 +78,28 @@ const SideNavBar = () => {
                 <li>Profile</li>
             </span>
             </Link>
-            <span className='inlineNav navHover'>
+            <span className='inlineNav navHover' onClick={()=>{setShow(true)}}>
                 <img src = {NewThotIcon} alt = '' style={{ backgroundColor: 'aqua', height: '35px', width: '35px' }} />
                 <li>New Thot</li>
             </span>
         </div>
+        <RandomlyPositionedModal show={show} onHide={()=>setShow(false)} renderBackdrop={renderBackdrop} autoFocus={show}> 
+            <div className='modal-container'>
+                <button className='close-modal' onClick={()=>setShow(false)}><CloseIcon /></button>
+                <form action='/post' method='post'>
+                <div className='modal-header'>
+                    <div style={{width: '50px', height: "200px"}}>
+                        <img src={maleavatar} alt="" className='modal-avatar' />
+                    </div>
+                    <textarea type='text' placeholder="What's Happening?" className='modal-input' maxLength="280" />
+                </div>
+                <div className='modal-thot'>
+                    <button className='newthot' type='submit'>Thot</button>
+                </div>
+                </form>
+            </div>
+        </RandomlyPositionedModal>
+        </>
     )
 }
 
