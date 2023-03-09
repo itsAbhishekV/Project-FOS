b8076e028cfd0d7569d642690b7cfe3dcc8f49da
Abhishek Verma <abhishek1006.be20@chitkarauniversity.edu.in>
2023-03-09 15:10:16 +0530

    9 march, 15:10 pm

--- src/Components/SignUp/SignUp.components.jsx
+++ src/Components/SignUp/SignUp.components.jsx
@@ -25,7 +25,7 @@ class SignUp extends React.Component{
 
     handleSubmit = (event) => {
         event.preventDefault();
-        //console.log(this.state)
+        console.log(this.state)
 
         const {password, confirmPassword} = this.state;
 
@@ -34,17 +34,18 @@ class SignUp extends React.Component{
             return;
         }
 
-        let obj = this.state;
-        fetch('/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(obj)
-        })
-        .then(response=>response.json())
-        .then(data=>console.log('success: ' + data))
-        .catch(err=>console.log(err))
+    //     let obj = this.state;
+    //     fetch('/signup', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(obj)
+    //     })
+    //     .then(response=>response.json())
+    //     .then(data=>console.log('success: ' + data))
+    //     .catch(err=>console.log(err))
+    // 
     }
 
     handleDateChange = (event) => {
